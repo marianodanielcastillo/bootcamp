85646200949dba8484a16d703456aa62b1e5b86a
Mariano Castillo <mariano.castillo@globant.com>
2017-01-24 15:03:22 -0300

    segundo, agregada promesa a funcion

--- js/plugins.js
+++ js/plugins.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$('#principal').fadeIn(5000)
-});
+});*/
 
 
-  function request(url) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+/*function request(url) {
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("principal").innerHTML =
       this.responseText;
@@ -13,4 +13,30 @@ $(document).ready(function(){
   };
   xhttp.open("GET", url, true);
   xhttp.send();
+}*/
+
+function request(method,url){
+	return new Promise(function (resolve,reject){
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url, true);
+		xhr.onload =function(){
+			if(this.status >=200 && this.status <=300){
+				resolve(xhr.response);
+				document.getElementById("principal").innerHTML =this.responseText;
+			}else{
+				reject({
+					status: this.status,
+					statusText : xhr.statusText
+				});
+			}
+		};
+		xhr.onerror =function (){
+			reject({
+				status: this.status,
+				statusText: xhr.statusText
+
+			});
+		};
+		xhr.send();
+	});
 }
